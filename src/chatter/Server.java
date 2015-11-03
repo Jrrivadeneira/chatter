f7e69d1fe234db0930c1934c421084c46e9be8b4
nebeinstein <nebeinstein@wpi.edu>
2015-11-03 09:24:17 -0500

    Created Server, added main fct

--- src/chatter/Server.java
+++ src/chatter/Server.java
@@ -1,5 +1,11 @@
 package chatter;
-import java.net.*;
-public class Server {
 
+import java.net.ServerSocket;
+/*
+ * We're probably going to use TCP?
+ */
+public class Server {
+	public static void main(String[] args) {
+
+	}
 }
